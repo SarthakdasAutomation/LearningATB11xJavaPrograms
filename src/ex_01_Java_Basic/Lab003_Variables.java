0e55c48316ff6f217613670ac06675c0eb63cbc4
Sarthak <sarthakdas899@gmail.com>
2025-03-29 03:54:11 +0530

    29-03: pushing variable example

---
+++ src/ex_01_Java_Basic/Lab003_Variables.java
@@ -0,0 +1,28 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Variables {
+    public static void main(String[] args) {
+        //different types of variable in java
+        boolean b=true; // or false,
+//        boolean b1=True; this will give error as True or False, caps is not applicable
+        System.out.println("Boolean: "+b);
+        byte b1=1; //range -128 to 127
+        System.out.println("byte: "+b1);
+        char c='a'; //value gets converted into ASCII, range 0 to 65535, single place only
+        //char c='1'; //this is also valid
+        System.out.println("Char: "+c);
+        short s=12456; //range -32768 to 32767
+        System.out.println("Short: "+s);
+        int i=1214344567; //range -2147483648 to 2147483647
+        System.out.println("int: "+i);
+        long l=1234567890123L; //L or l needed to be used
+        System.out.println("long: "+l);
+        float f=122.344567555555555f; //f or F need to be mentioned, print up to 5 places after decimal
+        System.out.println("Float: "+f);
+        double d=1223234.31233983424; //up to 10 places after decimal
+        System.out.println("Double: "+d);
+        System.out.printf("Decimal formating: %.7f%n",d); //formating up to 7 decimal places
+        String s1="Asd";
+        System.out.println("String: "+s1);
+    }
+}
